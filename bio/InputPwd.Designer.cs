5dcadb9551b7a53d77fe8a55c9e338d00d89180a
lawrence <lawrence@tzuchi>
2018-03-02 09:52:49 +0800

    20180302 - backup

--- bio/InputPwd.Designer.cs
+++ bio/InputPwd.Designer.cs
@@ -43,10 +43,9 @@
             this.lblVerPwd.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblVerPwd.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblVerPwd.ForeColor = System.Drawing.Color.White;
-            this.lblVerPwd.Location = new System.Drawing.Point(16, 134);
-            this.lblVerPwd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblVerPwd.Location = new System.Drawing.Point(12, 107);
             this.lblVerPwd.Name = "lblVerPwd";
-            this.lblVerPwd.Size = new System.Drawing.Size(115, 36);
+            this.lblVerPwd.Size = new System.Drawing.Size(86, 29);
             this.lblVerPwd.TabIndex = 0;
             this.lblVerPwd.Text = "密碼確認:";
             this.lblVerPwd.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -54,31 +53,29 @@
             // txtPWD
             // 
             this.txtPWD.Font = new System.Drawing.Font("微軟正黑體", 12F);
-            this.txtPWD.Location = new System.Drawing.Point(129, 134);
-            this.txtPWD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPWD.Location = new System.Drawing.Point(97, 107);
             this.txtPWD.Name = "txtPWD";
-            this.txtPWD.Size = new System.Drawing.Size(224, 34);
+            this.txtPWD.Size = new System.Drawing.Size(169, 29);
             this.txtPWD.TabIndex = 3;
             this.txtPWD.UseSystemPasswordChar = true;
+            this.txtPWD.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPWD_KeyPress);
             // 
             // lblRemind
             // 
             this.lblRemind.AutoSize = true;
             this.lblRemind.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblRemind.ForeColor = System.Drawing.Color.Black;
-            this.lblRemind.Location = new System.Drawing.Point(16, 48);
-            this.lblRemind.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRemind.Location = new System.Drawing.Point(12, 38);
             this.lblRemind.Name = "lblRemind";
-            this.lblRemind.Size = new System.Drawing.Size(0, 25);
+            this.lblRemind.Size = new System.Drawing.Size(0, 21);
             this.lblRemind.TabIndex = 4;
             // 
             // btnVer
             // 
             this.btnVer.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnVer.Location = new System.Drawing.Point(129, 219);
-            this.btnVer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVer.Location = new System.Drawing.Point(97, 175);
             this.btnVer.Name = "btnVer";
-            this.btnVer.Size = new System.Drawing.Size(99, 38);
+            this.btnVer.Size = new System.Drawing.Size(74, 30);
             this.btnVer.TabIndex = 5;
             this.btnVer.Text = "確認";
             this.btnVer.UseVisualStyleBackColor = true;
@@ -87,10 +84,9 @@
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnCancel.Location = new System.Drawing.Point(249, 219);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Location = new System.Drawing.Point(187, 175);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(105, 38);
+            this.btnCancel.Size = new System.Drawing.Size(79, 30);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -102,10 +98,9 @@
             this.lblID.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblID.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.lblID.ForeColor = System.Drawing.Color.White;
-            this.lblID.Location = new System.Drawing.Point(13, 48);
-            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Location = new System.Drawing.Point(10, 38);
             this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(115, 36);
+            this.lblID.Size = new System.Drawing.Size(86, 29);
             this.lblID.TabIndex = 7;
             this.lblID.Text = "ID :";
             this.lblID.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -114,18 +109,19 @@
             // txtID
             // 
             this.txtID.Font = new System.Drawing.Font("微軟正黑體", 12F);
-            this.txtID.Location = new System.Drawing.Point(129, 48);
+            this.txtID.Location = new System.Drawing.Point(97, 38);
+            this.txtID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(224, 34);
+            this.txtID.Size = new System.Drawing.Size(169, 29);
             this.txtID.TabIndex = 8;
             this.txtID.Visible = false;
             // 
             // frmVerPwd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(419, 271);
+            this.ClientSize = new System.Drawing.Size(314, 217);
             this.ControlBox = false;
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.lblID);
@@ -134,7 +130,6 @@
             this.Controls.Add(this.lblRemind);
             this.Controls.Add(this.txtPWD);
             this.Controls.Add(this.lblVerPwd);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmVerPwd";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- bio/InputPwd.cs
+++ bio/InputPwd.cs
@@ -22,117 +22,7 @@ namespace BioBank
 
         private void btnVer_Click(object sender, EventArgs e)
         {
-            string sSQL = "";
-            string sPwd = "";
-            string sID = "";
-            string sCorrectPwd = "";
-            PassVerPwd = false;
-
-            sPwd = txtPWD.Text;
-            sID = ClsShareFunc.sUserId;
-
-
-            if (sPwd == "")
-            {
-                MessageBox.Show("請輸入密碼!");
-                return;
-            }
-           
-                //switch (ClsShareFunc.sLoginIdentity)
-            if (pEntrySource == "Function6" || pEntrySource == "Function7" || pEntrySource == "Function10")
-                {
-                    if (ClsShareFunc.sLoginIdentity != "Administrator")
-                    {                       
-                        MessageBox.Show("非【生物、資訊主管-Administrator】權限，無法進入！", "Administrator Only!!!");
-                        this.Close();
-                    }
-                    else
-                    {
-                        //using (SqlConnection sCon = new SqlConnection(ClsShareFunc.DB_SECConnection()))
-                        using (SqlConnection sCon = BioBank_Conn.Class_biobank_conn.DB_SEC_conn())
-                        {
-                            sCon.Open();
-                            sSQL = " select * from BioAdministratorKeyTbl (nolock) where chUserID = '" + sID + "' ";
-                            SqlCommand sCmd = new SqlCommand(sSQL, sCon);
-                            SqlDataReader sRead = sCmd.ExecuteReader();
-
-                            if (sRead.HasRows == true)
-                            {
-                                while (sRead.Read())
-                                {
-                                    sCorrectPwd = ClsShareFunc.gfunCheck(sRead["chAdministratorKey"]);
-                                }
-                                sRead.Close();
-
-                                if (sCorrectPwd == ClsShareFunc.GetMD5(sPwd))
-                                {
-                                    PassVerPwd = true;
-                                    this.Close();
-                                }
-                                else
-                                {
-                                    PassVerPwd = false;
-                                    MessageBox.Show("密碼錯誤，請重新輸入！");
-                                    txtPWD.Text = "";
-                                }
-                            }
-                        }  
-                    }
-                }
-                if (pEntrySource == "Function8")
-                {
-                    if (ClsShareFunc.sLoginIdentity != "Common")
-                    {
-                        MessageBox.Show("需先以一般行政同仁權限進入【再輔以生物、資訊主管-Administrator 權限進入】！", "行政同仁 First!!!");
-                        return;
-                    }
-                    else
-                    {
-                        if (txtID.Text.Trim() == "" || txtPWD.Text.Trim() == "")
-                        {
-                            MessageBox.Show("ID 及 PWD不可為空白!");
-                            return;
-                        }
-                        //using (SqlConnection sCon = new SqlConnection(ClsShareFunc.DB_SECConnection()))
-                        using (SqlConnection sCon = BioBank_Conn.Class_biobank_conn.DB_SEC_conn())
-                        {
-                            sCon.Open();
-                            sSQL = " select * from BioAdministratorKeyTbl (nolock) where chUserID = '" + txtID.Text.Trim()   + "' ";
-                            SqlCommand sCmd = new SqlCommand(sSQL, sCon);
-                            SqlDataReader sRead = sCmd.ExecuteReader();
-
-                            if (sRead.HasRows == true)
-                            {
-                                while (sRead.Read())
-                                {
-                                    sCorrectPwd = ClsShareFunc.gfunCheck(sRead["chAdministratorKey"]);
-                                }
-                                sRead.Close();
-
-                                if (sCorrectPwd == ClsShareFunc.GetMD5(sPwd))
-                                {
-                                    PassVerPwd = true;
-                                    BioBank.pFunction8_AdminID = txtID.Text.Trim();
-                                    this.Close();
-                                }
-                                else
-                                {
-                                    PassVerPwd = false;
-                                    MessageBox.Show("密碼錯誤，請重新輸入！");
-                                    txtPWD.Text = "";
-                                }
-                            }
-                            else
-                            {
-                                PassVerPwd = false;
-                                MessageBox.Show("ID 或 密碼錯誤，請重新輸入！");
-                                txtPWD.Text = "";
-                            }
-
-                        }
-                    }
-
-                }
+            checkPwd();
         }
 
         private void frmVerPwd_Load(object sender, EventArgs e)
@@ -179,6 +69,129 @@ namespace BioBank
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtPWD_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(13))
+            {
+                checkPwd();
+            }
+        }
+
+        private void checkPwd()
+        {
+            string sSQL = "";
+            string sPwd = "";
+            string sID = "";
+            string sCorrectPwd = "";
+            PassVerPwd = false;
+
+            sPwd = txtPWD.Text;
+            sID = ClsShareFunc.sUserId;
+
+
+            if (sPwd == "")
+            {
+                MessageBox.Show("請輸入密碼!");
+                return;
+            }
+
+            //switch (ClsShareFunc.sLoginIdentity)
+            if (pEntrySource == "Function6" || pEntrySource == "Function7" || pEntrySource == "Function10")
+            {
+                if (ClsShareFunc.sLoginIdentity != "Administrator")
+                {
+                    MessageBox.Show("非【生物、資訊主管-Administrator】權限，無法進入！", "Administrator Only!!!");
+                    this.Close();
+                }
+                else
+                {
+                    //using (SqlConnection sCon = new SqlConnection(ClsShareFunc.DB_SECConnection()))
+                    using (SqlConnection sCon = BioBank_Conn.Class_biobank_conn.DB_SEC_conn())
+                    {
+                        sCon.Open();
+                        sSQL = " select * from BioAdministratorKeyTbl (nolock) where chUserID = '" + sID + "' ";
+                        SqlCommand sCmd = new SqlCommand(sSQL, sCon);
+                        SqlDataReader sRead = sCmd.ExecuteReader();
+
+                        if (sRead.HasRows == true)
+                        {
+                            while (sRead.Read())
+                            {
+                                sCorrectPwd = ClsShareFunc.gfunCheck(sRead["chAdministratorKey"]);
+                            }
+                            sRead.Close();
+
+                            if (sCorrectPwd == ClsShareFunc.GetMD5(sPwd))
+                            {
+                                PassVerPwd = true;
+                                this.Close();
+                            }
+                            else
+                            {
+                                PassVerPwd = false;
+                                MessageBox.Show("密碼錯誤，請重新輸入！");
+                                txtPWD.Text = "";
+                            }
+                        }
+                    }
+                }
+            }
+            if (pEntrySource == "Function8")
+            {
+                if (ClsShareFunc.sLoginIdentity != "Common")
+                {
+                    MessageBox.Show("需先以一般行政同仁權限進入【再輔以生物、資訊主管-Administrator 權限進入】！", "行政同仁 First!!!");
+                    return;
+                }
+                else
+                {
+                    if (txtID.Text.Trim() == "" || txtPWD.Text.Trim() == "")
+                    {
+                        MessageBox.Show("ID 及 PWD不可為空白!");
+                        return;
+                    }
+                    //using (SqlConnection sCon = new SqlConnection(ClsShareFunc.DB_SECConnection()))
+                    using (SqlConnection sCon = BioBank_Conn.Class_biobank_conn.DB_SEC_conn())
+                    {
+                        sCon.Open();
+                        sSQL = " select * from BioAdministratorKeyTbl (nolock) where chUserID = '" + txtID.Text.Trim() + "' ";
+                        SqlCommand sCmd = new SqlCommand(sSQL, sCon);
+                        SqlDataReader sRead = sCmd.ExecuteReader();
+
+                        if (sRead.HasRows == true)
+                        {
+                            while (sRead.Read())
+                            {
+                                sCorrectPwd = ClsShareFunc.gfunCheck(sRead["chAdministratorKey"]);
+                            }
+                            sRead.Close();
+
+                            if (sCorrectPwd == ClsShareFunc.GetMD5(sPwd))
+                            {
+                                PassVerPwd = true;
+                                BioBank.pFunction8_AdminID = txtID.Text.Trim();
+                                this.Close();
+                            }
+                            else
+                            {
+                                PassVerPwd = false;
+                                MessageBox.Show("密碼錯誤，請重新輸入！");
+                                txtPWD.Text = "";
+                            }
+                        }
+                        else
+                        {
+                            PassVerPwd = false;
+                            MessageBox.Show("ID 或 密碼錯誤，請重新輸入！");
+                            txtPWD.Text = "";
+                        }
+
+                    }
+                }
+
+            }    
         }
     }
 }
